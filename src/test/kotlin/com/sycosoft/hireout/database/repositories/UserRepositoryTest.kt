047573121b47502e81f94fea81e2ba875f040456
JamieRhys <jamie_airfix@yahoo.co.uk>
2024-03-30 13:58:14 +0000

    Updated RepositoryTest

--- src/test/kotlin/com/sycosoft/hireout/database/repositories/UserRepositoryTest.kt
+++ src/test/kotlin/com/sycosoft/hireout/database/repositories/UserRepositoryTest.kt
@@ -42,31 +42,31 @@ class UserRepositoryTest {
 
 //region Save Method tests
 
-    @Test
-    fun givenUserObject_whenSaved_thenReturnSavedUser() {
-//        val savedUser = userRepository.save(User.Builder()
-//            .firstName(TestStrings.USER_FIRST_NAME_VALID)
-//            .lastName(TestStrings.USER_LAST_NAME_VALID)
-//            .username(TestStrings.USER_USERNAME_VALID)
-//            .password(TestStrings.USER_PASSWORD_VALID)
-//            .isDeleted(false)
-//            .build())
-//
-//        assertNotNull(savedUser)
-//        assertNotNull(savedUser.uuid)
-//        assertEquals(savedUser.firstName, TestStrings.USER_FIRST_NAME_VALID)
-//        assertEquals(savedUser.lastName, TestStrings.USER_LAST_NAME_VALID)
-//        assertEquals(savedUser.username, TestStrings.USER_USERNAME_VALID)
-//        assertFalse(savedUser.isDeleted)
-    }
+//    @Test
+//    fun givenUserObject_whenSaved_thenReturnSavedUser() {
+////        val savedUser = userRepository.save(User.Builder()
+////            .firstName(TestStrings.USER_FIRST_NAME_VALID)
+////            .lastName(TestStrings.USER_LAST_NAME_VALID)
+////            .username(TestStrings.USER_USERNAME_VALID)
+////            .password(TestStrings.USER_PASSWORD_VALID)
+////            .isDeleted(false)
+////            .build())
+////
+////        assertNotNull(savedUser)
+////        assertNotNull(savedUser.uuid)
+////        assertEquals(savedUser.firstName, TestStrings.USER_FIRST_NAME_VALID)
+////        assertEquals(savedUser.lastName, TestStrings.USER_LAST_NAME_VALID)
+////        assertEquals(savedUser.username, TestStrings.USER_USERNAME_VALID)
+////        assertFalse(savedUser.isDeleted)
+//    }
 
 //endregion
 //region Get Method Tests
 
-    @Test
-    fun givenValidUUID_whenGetting_thenReturnUserObject() {
-
-    }
+//    @Test
+//    fun givenValidUUID_whenGetting_thenReturnUserObject() {
+//
+//    }
 
 //endregion
 }
